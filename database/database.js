9996bacac46d263f06e21864b10b3a448c6cc5a5
Ryan Maier <ryandmaier@gmail.com>
2021-11-29 14:41:13 -0800

    Searchbar filter and most of sidebar filter
    Sidebar filter still needs data posted filters
    Also still need to merge search bar and sidebar filters so they can be used at the same time

--- database/database.js
+++ database/database.js
@@ -3,10 +3,10 @@
 
 const mongoose = require("mongoose");
 const ProductSchema = require("./productSchema");
-// const UserSchema = require("./userSchema");
+const UserSchema = require("./userSchema");
 const process = require("process");
 const dotenv = require("dotenv");
-dotenv.config();
+dotenv.config({ path: "" }); // insert path to .env here
 
 let conn;
 
@@ -45,131 +45,129 @@ function getConnection() {
 	return conn;
 }
 
-let prodCriteria = [
-	"_id",
-	"title",
-	"datePosted",
-	"categories",
-	"description",
-	"condition",
-	"location",
-	"seller",
-];
-
-/**
- * @param {*} criteria should be a list of 5 strings, where each element
- *     corresponds to the search criteria in the order above. Criteria
- *     which are not being filtered should be represented by empty strings.
- * Examples:
- * ["", "", "", "", "", "", ""] // returns all products
- * ["", "", "", "", "", "new", "", ""] // returns all condition = "new" products
- * ["", "Toy", "", "", "", "", "", "Dave"] // returns all title = "Toy" products posted by Dave
- * ["abc123", "Toy", "", "", "", "", "", "Dave"] // returns product _id = "abc123", no matter the other criteria
- */
 async function getProducts(criteria) {
+	// change to accept search and sidebar criteria, filter by sidebar and then searchs
+
+	if (Array.isArray(criteria)) {
+		// it's a sidebar filter
+		return getProductsSidebar(criteria);
+	} else {
+		// it's a search bar filter
+		return getProductsSearch(criteria);
+	}
+}
+
+// criteriaList = [“_id”, "condition", "category", "date posted"]
+async function getProductsSidebar(criteriaList) {
 	const productModel = getConnection().model("Product", ProductSchema);
 	let products;
+	let filter = 0;
 	// if _id not in search criteria, filter by other criteria
-	if (criteria[0] === "") {
-		products = await productModel.find();
-		return filterProducts(products, criteria);
-	} else {
-		// else return product with that _id
-		return productModel.findById(criteria[0]);
+	if (!(criteriaList[0] === "")) {
+		// return product with that _id
+		return productModel.findById(criteriaList[0]);
 	}
+	let find = "";
+	let q = {};
+	if (!(criteriaList[1] === "")) {
+		// add condition to query
+		find = find + " condition: " + criteriaList[1] + ",";
+		q = Object.assign(q, { condition: criteriaList[1] });
+		filter = 1;
+	}
+	if (!(criteriaList[2] === "")) {
+		// add category to query
+		find = find + " category: " + criteriaList[2];
+		q = Object.assign(q, { category: criteriaList[2] });
+		filter = 1;
+	}
+
+	if (filter === 1) products = await productModel.find(q);
+	else products = await productModel.find();
+
+	if (!(criteriaList[3] === "")) {
+		// filter by date posted
+		products = filterByDate(products, criteriaList[3]);
+	}
+	return products;
+}
+
+function filterByDate(products, date) {
+	// to be implemented next
+}
+
+async function getProductsSearch(searchString) {
+	const productModel = getConnection().model("Product", ProductSchema);
+	products = await productModel.find();
+	return filterProductsSearch(products, searchString);
+}
+
+function filterProductsSearch(products, searchString) {
+	// break search into individual words
+	let searchWords = searchString.split(" ");
+	// list of {product, score} tuples
+	prodScores = [];
+	products.forEach((prod) => {
+		// complie product keywords from title, description, seller, location, etc.
+		let sellerName = getSellerName(prod.seller); // get seller name from product's user _id
+		let prodStrs = [prod.title, sellerName, prod.description];
+		// list to accumulate points for each product search attribute
+		let rs = [0, 0, 0];
+		// compare each keyword to search and update product's score
+		for (let i = 0; i < prodStrs.length; i++) {
+			// compare to each prod str
+			let prodWords = prodStrs[i].split(" ");
+			prodWords.forEach((pword) => {
+				searchWords.forEach((sword) => {
+					// calculate Levenshtein distance
+					let lev = dynamicSimilarity(
+						pword.toLowerCase(),
+						sword.toLowerCase()
+					);
+					rs[i] += lev;
+					// if (lev > threshold) rs[i] += 1;
+				});
+			});
+		}
+		// weight rs accordingly
+		let points = 10 * rs[0] + 6 * rs[1] + 9 * rs[2];
+		// add tripple to list
+		prodScores.push({ product: prod, score: points });
+	});
+
+	// sort products by their respective search scores
+	let x = prodScores.sort((a, b) => (a.score < b.score ? 1 : -1));
+
+	// compile a list of just the projects in their sorted order
+	prodsSorted = [];
+	prodScores.forEach((p) => {
+		prodsSorted.push(p.product);
+	});
+
+	return prodsSorted;
+}
+
+function getSellerName(id) {
+	// findById to get user name from the product's seller _id
+	// const userModel = getConnection().model("User", UserSchema);
+	// return userModel.getUsers(id);
+	return id;
+
+	// // // // // // // // // // // // // // // // // // // // // // // //
 }
 
 /**
- * Would like to implement "contains" for criteria
- * @param {*} products
- * @param {*} criteria
- * for each criteria
- * 		for each product
- * 			if similar, don't remove prod
+ * Use Levenshtein distance to calculate the similarity between 2 strings
+ * @param {C} s1 : first String
+ * @param {*} s2 : second String
  */
-async function filterProducts(products, criteria) {
-	// shallow copy products
-	let prods = Array.from(products);
-	// define variables
-	let prodsThatPass = [];
-	let pVals = [];
-	let pVal = "";
-	// for each product excluding _id (i==0)
-	for (let i = 1; i < criteria.length; i++) {
-		// if this criteria is being searched for
-		if (!(criteria[i] === "")) {
-			// for each prod in prods, which is updated after each criteria filtering
-			prods.forEach((prod) => {
-				// have to fetch all product values and then choose the one matching this criteria
-				pVals = [
-					prod.title,
-					prod.datePosted,
-					prod.categories,
-					prod.description,
-					prod.condition,
-					prod.seller,
-				];
-				pVal = pVals[i - 1];
-				const similar = dynamicSimilarity(
-					String(pVal),
-					String(criteria[i])
-				);
-				// add semi in order to array, much better ordering can be designed later
-				console.log(
-					String(pVal) +
-						" and " +
-						String(criteria[i]) +
-						" : " +
-						similar +
-						"\n\n"
-				);
-				if (similar > 0.75) {
-					prodsThatPass.unshift(prod); // add best ones to front
-				}
-				if (similar > 0.6) {
-					prodsThatPass.push(prod); // add not best ones to end
-				}
-			});
-			// update product list, reverse so that worst are added to front first
-			prods = Array.from(prodsThatPass.reverse());
-			// clear prodsThatPass for next criteria filtering
-			prodsThatPass = [];
-		}
-	}
-	return prods;
-}
-
-function editDistance(s1, s2) {
-	s1 = s1.toLowerCase();
-	s2 = s2.toLowerCase();
-	console.log("Compare " + s1 + " vs " + s2);
-	var costs = new Array();
-	for (var i = 0; i <= s1.length; i++) {
-		var lastValue = i;
-		for (var j = 0; j <= s2.length; j++) {
-			if (i == 0) costs[j] = j;
-			else {
-				if (j > 0) {
-					var newValue = costs[j - 1];
-					if (s1.charAt(i - 1) != s2.charAt(j - 1))
-						newValue =
-							Math.min(Math.min(newValue, lastValue), costs[j]) +
-							1;
-					costs[j - 1] = lastValue;
-					lastValue = newValue;
-				}
-			}
-		}
-		if (i > 0) costs[s2.length] = lastValue;
-	}
-	let longerLength = Math.max(s1.length, s2.length);
-	let ed = (longerLength - costs[s2.length]) / longerLength;
-	console.log(ed);
-	return ed;
-}
-
 function dynamicSimilarity(s1, s2) {
+	// if they are the same, return an extra high similarity
+	if (s1 === s2) {
+		return 2;
+	}
 	let array = [];
+	// build 2d array with the right shape and size
 	for (let i = 0; i < s1.length + 1; i++) {
 		let ar = [];
 		for (let j = 0; j < s2.length + 1; j++) {
@@ -177,38 +175,46 @@ function dynamicSimilarity(s1, s2) {
 		}
 		array.push(ar);
 	}
+	// fill in table with Levenshtein distances
 	for (let i = 0; i < s1.length + 1; i++) {
 		for (let j = 0; j < s2.length + 1; j++) {
 			if (i == 0) array[i][j] = j;
-			else if (j == 0) array[i][i] = i;
+			else if (j == 0) array[i][j] = i;
 			else {
 				let o1 = array[i][j - 1] + 1;
 				let o2 = array[i - 1][j] + 1;
 				let o3 = array[i - 1][j - 1];
-				if (!(s1.charAt(i) === s2.charAt(j))) o3 += 1;
+				if (!(s1.charAt(i - 1) === s2.charAt(j - 1))) o3 += 1;
 				array[i][j] = Math.min(o1, o2, o3);
 			}
 		}
 	}
+	// calculate % similarity
 	let len = Math.max(s1.length, s2.length);
 	let ed = array[s1.length][s2.length];
-	return 1 - ed / (len + 1);
+	return (len - ed) / len;
 }
 
-async function getUsers() {
+async function getUsers(id) {
 	const userModel = getConnection().model("User", UserSchema);
-	let users = await userModel.find();
-	return users;
+	if (!(id === "")) {
+		// find user by id
+		return await userModel.findById(id); // might need to convert from string to ObjectID type
+	} else {
+		return await userModel.find();
+	}
 }
 
 /**
  * db.products.insertOne({title: "Bench", datePosted: "11/01/2021", productID: "abc124", categories: ["sit","sturdy","furniture","wood"], description: "It's a good bench", condition: "used", seller: "peteroustem"})
+ * Verify setting product seller as user._id implementation
  * @param {*} product
  */
-async function addProduct(product) {
+async function addProduct(product, user) {
 	const productModel = getConnection().model("Product", ProductSchema);
 	try {
 		const prodToAdd = new productModel(product);
+		prodToAdd.seller = user._id;
 		const savedProd = await prodToAdd.save();
 		return savedProd;
 	} catch (error) {
